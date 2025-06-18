d67c42a6950926184298892f73b09c3c7fbe08b7
Steff0 <carspotter.wt@gmail.com>
2025-06-18 18:59:34 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+"""
+Modulo models per il sistema di predizione azionaria
+Contiene le classi e funzioni per i modelli di machine learning
+"""
 
+from .ensemble_predictor import EnsemblePredictor
+from .technical_indicators import TechnicalIndicators
+from .market_analyzer import MarketAnalyzer
+
+__version__ = "1.0.0"
+__author__ = "Stock Prediction System"
+
+# Esporta le classi principali
+__all__ = [
+    'EnsemblePredictor',
+    'TechnicalIndicators', 
+    'MarketAnalyzer'
+]
