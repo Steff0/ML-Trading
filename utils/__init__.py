3e050798d5efe7f9b0af3af208d7f5ef0a753f74
Steff0 <carspotter.wt@gmail.com>
2025-06-18 18:55:31 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,34 @@
-utils/
+
+#!/usr/bin/env python3
+"""
+Modulo utils per il sistema di predizione azionaria
+Contiene funzioni di utilità, helper e configurazioni
+"""
+
+from .data_validator import DataValidator
+from .database_manager import DatabaseManager
+from .logging_config import setup_logging
+from .config import Config
+from .helpers import (
+    calculate_returns,
+    normalize_data,
+    validate_symbol,
+    format_currency,
+    get_trading_days
+)
+
+__version__ = "1.0.0"
+__author__ = "Stock Prediction System"
+
+# Esporta le classi e funzioni principali
+__all__ = [
+    'DataValidator',
+    'DatabaseManager',
+    'setup_logging',
+    'Config',
+    'calculate_returns',
+    'normalize_data',
+    'validate_symbol',
+    'format_currency',
+    'get_trading_days'
+]
